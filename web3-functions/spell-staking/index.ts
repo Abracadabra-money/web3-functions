0d6e18912909bcfd4f1d19edc204848a61c86862
0xCalibur <0xCalibur@protonmail.com>
2023-06-15 18:03:03 -0400

    wip

--- web3-functions/spell-staking/index.ts
+++ web3-functions/spell-staking/index.ts
@@ -4,7 +4,7 @@ import {
 } from "@gelatonetwork/web3-functions-sdk";
 import { BigNumber, Contract } from "ethers";
 
-const ORACLE_ABI = [
+const WITHDRAWER_ABI = [
   "function getStoredPrice() public view returns (uint256)",
   "function getLivePrice() public view returns (uint256)",
   "function getPriceDeviation() external view returns (uint)",
@@ -12,17 +12,39 @@ const ORACLE_ABI = [
   "function updatePrice() public",
 ];
 
+const DISTRIBUTOR_ABI = [
+];
+
 Web3Function.onRun(async (context: Web3FunctionContext) => {
   const { userArgs, multiChainProvider } = context;
+
+  const withdrawerAddress = "0x2C9f65BD1a501CB406584F5532cE57c28829B131";
+  const distributorAddress = "0x953DAb0e64828972853E7faA45634620A40Fa479";
+
+  const withdrawers: { [chainId: number]: Contract } = {};
+
+  const chainIds = [1, 250, 43114, 42161];
+
+  chainIds.forEach(async (chainId) => {
+    const provider = multiChainProvider.chainId(chainId);
+
+    const withdrawer = new Contract(
+      withdrawerAddress,
+      WITHDRAWER_ABI,
+      provider
+    );
+    withdrawers[chainId] = withdrawer;
+  });
+
+  const distributor = new Contract(distributorAddress, DISTRIBUTOR_ABI, multiChainProvider.chainId(1));
 
   const provider = multiChainProvider.chainId(1);
   const chainId = await (await provider.getNetwork()).chainId;
   const blockHeight = await provider.getBlockNumber();
-  console.log(chainId, blockHeight);
 
   /*const oracleAddress = userArgs.oracleAddress as string;
   const oracle = new Contract(oracleAddress, ORACLE_ABI, provider);
-
+ 
   // Wrap contract with redstone data service
   const wrappedOracle = WrapperBuilder.wrap(oracle).usingDataService(
     {
@@ -33,19 +55,19 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
     },
     ["https://d33trozg86ya9x.cloudfront.net"]
   );
-
+ 
   // Retrieve stored & live prices
   const decimals = await wrappedOracle.decimals();
   const livePrice: BigNumber = await wrappedOracle.getLivePrice();
   const storedPrice: BigNumber = await wrappedOracle.getStoredPrice();
   console.log(`Live price: ${livePrice.toString()}`);
   console.log(`Stored price: ${storedPrice.toString()}`);
-
+ 
   // Check price deviation
   const deviation: BigNumber = await wrappedOracle.getPriceDeviation();
   const deviationPrct = (deviation.toNumber() / 10 ** decimals) * 100;
   console.log(`Deviation: ${deviationPrct.toFixed(2)}%`);
-
+ 
   // Only update price if deviation is above 0.2%
   const minDeviation = 0.2;
   if (deviationPrct < minDeviation) {
@@ -54,10 +76,10 @@ Web3Function.onRun(async (context: Web3FunctionContext) => {
       message: `No update: price deviation too small`,
     };
   }
-
+ 
   // Craft transaction to update the price on-chain
   const { data } = await wrappedOracle.populateTransaction.updatePrice();
-
+ 
   */
   return {
     canExec: true,
